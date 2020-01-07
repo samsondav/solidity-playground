abfbbcbab419809b714c64b75de0b3697e338641
Sam Davies <sampdavies@gmail.com>
2020-01-07 12:47:06 +0700

    Add events

--- contracts/Faucet.sol
+++ contracts/Faucet.sol
@@ -1,13 +1,21 @@
 pragma solidity ^0.4.24;
 
-contract Faucet {
+import './lib/Mortal.sol';
+
+contract Faucet is Mortal {
     uint constant MAX_WITHDRAWAL = 0.1 ether;
     // Give out ether to anybody who asks
+
+    event Withdrawal(address indexed to, uint amount);
+    event Deposit(address indexed from, uint amount);
 
     function withdraw(uint _amount) public {
         require(_amount <= MAX_WITHDRAWAL, "don't be greedy!");
         msg.sender.transfer(_amount);
+        emit Withdrawal(msg.sender, _amount);
     }
 
-    function () public payable {}
+    function () public payable {
+        emit Deposit(msg.sender, msg.value);
+    }
 }
